8eeafab146fe3f811845cb482a84ce500d75e265
Muhammed Shamal <shamal079779@gmail.com>
2021-01-21 19:16:46 +0530

    Added intro part

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-// jshint esversion:6
+// jshint esversion:8
 const result = document.getElementById('result');
 const yourScore = document.querySelector('.your-score');
 const computerScore = document.querySelector('.computer-score');
 const btn = document.querySelectorAll('.btn');
+const welcome = document.querySelector('.welcome');
+const userName = document.getElementById('user-name');
+const game = document.querySelector('.game-board');
+const name = document.getElementById('name-tag');
+const form = document.getElementById('form');
+const img = document.querySelectorAll('.img');
+
 let yScore = 0;
 let cScore = 0;
 let computerChoice = '';
 
-const  computerOption = () => {
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+        welcome.hidden = true;
+        name.innerText = userName.value;
+        game.style.display = "block";
+    
+});
+
+const computerOption = () => {
     let randomNumber = Math.floor(Math.random() * 3);
     
     if(randomNumber === 0){
@@ -20,10 +35,20 @@ const  computerOption = () => {
     return computerChoice;
 };
 
+
+
 for(let i = 0; i < btn.length; i++){
     btn[i].addEventListener('click', function() {
+        for(let j = 0; j < img.length; j++){
+            img[j].classList.remove('apply-shake'); 
+        }
     let cmp = computerOption();
     if(cmp == this.id){
+        for(let j = 0; j < img.length; j++){
+            if(img[j].classList.contains(cmp)){
+                img[j].classList.toggle('apply-shake');
+            }
+        }
         result.innerText = "Tie";
     } else if(cmp == 'stone' && this.id == 'paper'){
         result.innerText = "Paper covers the stone you win.";
